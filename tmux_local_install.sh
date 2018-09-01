979d6407c9f219985c0b4f6820b6bb775ecef632
jianrenw <42852521+jianrenw@users.noreply.github.com>
2018-09-01 14:16:26 -0400

    add bash file

---
+++ tmux_local_install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Script for installing tmux on systems where you don't have root access.
+# tmux will be installed in $HOME/local/bin.
+# It's assumed that wget and a C/C++ compiler are installed.
+
+# exit on error
+set -e
+
+# create our directories
+mkdir -p $HOME/local $HOME/tmux_tmp
+cd $HOME/tmux_tmp
+
+# download source files for tmux, libevent, and ncurses
+wget https://github.com/tmux/tmux/archive/master.zip
+wget --no-check-certificate https://github.com/jianrenw/tmux_w-o_root/blob/master/libevent-2.1.8-stable.tar.gz
+wget ftp://ftp.invisible-island.net/ncurses/ncurses.tar.gz
+
+# extract files, configure, and compile
+
+############
+# libevent #
+############
+tar xvzf libevent-2.1.8-stable.tar.gz
+cd libevent-2.1.8-stable
+./configure --prefix=$HOME/local --disable-shared
+make
+make install
+cd ..
+
+############
+# ncurses  #
+############
+tar xvzf ncurses.tar.gz
+cd ncurses-6.1
+./configure --prefix=$HOME/local
+make
+make install
+cd ..
+
+############
+# tmux     #
+############
+unzip master.zip
+cd tmux-master
+sh autogen.sh
+./configure CFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-L$HOME/local/lib -L$HOME/local/include/ncurses -L$HOME/local/include"
+CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-static -L$HOME/local/include -L$HOME/local/include/ncurses -L$HOME/local/lib" make
+cp tmux $HOME/local/bin
+cd ..
+
+# cleanup
+rm -rf $HOME/tmux_tmp
+
+echo "$HOME/local/bin/tmux is now available. You can optionally add $HOME/local/bin to your PATH."
